a3340b6a7757f7c56b888cf6ae303a042b1a2de0
vastral <78974721+vastral@users.noreply.github.com>
2024-12-03 03:24:08 +0100

    modify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist', // Carpeta donde se generarán los archivos de producción
   },
+  server: {
+    fs: {
+      strict: false, // Permite servir archivos fuera de la raíz del proyecto si es necesario
+    },
+  },
+  base: './', // Asegúrate de que la base esté configurada para rutas relativas
 });
+
